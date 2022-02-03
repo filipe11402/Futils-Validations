85538112d0167ddd3b8a41345d7c7fa8ce38fa26
filipe11402 <filipe11402@outlook.pt>
2022-02-03 23:22:34 +0000

    added validation attribute for master card credit cards but failing unit tests

---
+++ UtilsValidation.Tests/CreditCards/MasterCardTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using UtilsValidations.Src.CreditCards;
+using Xunit;
+
+namespace UtilsValidation.Tests.CreditCards
+{
+    public class MasterCardTests
+    {
+
+        [Theory]
+        [InlineData("5425233430109903")]
+        public void IsValid_MasterCardNumber_ReturnsTrue(object input) 
+        {
+            //Arrange
+            var validationContext = new ValidationContext(input);
+
+            //Act
+            var masterCardAnotation = new MasterCard();
+            var validationResult = masterCardAnotation.GetValidationResult(input, validationContext);
+
+            //Assert
+            Assert.True(Validator.TryValidateObject(input, validationContext, new List<ValidationResult> { validationResult }));
+        }
+
+        [Theory]
+        [InlineData("378282246310005")]
+        public void IsValid_MasterCardNumber_ReturnsFalse(object input)
+        {
+            //Arrange
+            var validationContext = new ValidationContext(input);
+
+            //Act
+            var masterCardAnotation = new MasterCard();
+            var validationResult = masterCardAnotation.GetValidationResult(input, validationContext);
+
+            //Assert
+            Assert.False(Validator.TryValidateObject(input, validationContext, new List<ValidationResult> { validationResult }));
+        }
+    }
+}
---
+++ UtilsValidations.Src/CreditCards/MasterCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace UtilsValidations.Src.CreditCards
+{
+    public class MasterCard : ValidationAttribute
+    {
+        public MasterCard()
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!Regex.IsMatch(value.ToString(), @"^5[1-5][0-9]{14}|^(222[1-9]|22[3-9]\\d|2[3-6]\\d{2}|27[0-1]\\d|2720)[0-9]{12}$"))
+            {
+                return new ValidationResult("Invalid");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
