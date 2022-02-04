da7343fbb8e823e3d0db5ff8d0265e23652186fd
filipe11402 <filipe11402@outlook.pt>
2022-02-04 22:07:38 +0000

    added Visa credit card number validation attribute with unit tests

---
+++ UtilsValidation.Tests/CreditCards/VisaTests.cs
@@ -0,0 +1,42 @@
+﻿using UtilsValidations.Src.CreditCards;
+using Xunit;
+
+namespace UtilsValidation.Tests.CreditCards
+{
+    public class VisaTests
+    {
+        [Theory]
+        [InlineData("4012888888881881")]
+        [InlineData("4012888888881881")]
+        [InlineData("4111111111111111")]
+        [InlineData("4002690000000008")]
+        public void IsValid_VisaCardNumber_ReturnsTrue(string input) 
+        {
+            //Arrange
+            var visaValidation = new Visa();
+
+            //Act
+            var result = visaValidation.IsValid(input);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("38520000023237")]
+        [InlineData("3530111333300000")]
+        [InlineData("6011111111111117")]
+        [InlineData("3566002020360505")]
+        public void IsValid_VisaCardNumber_ReturnsFalse(string input)
+        {
+            //Arrange
+            var visaValidation = new Visa();
+
+            //Act
+            var result = visaValidation.IsValid(input);
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
---
+++ UtilsValidations.Src/CreditCards/Visa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace UtilsValidations.Src.CreditCards
+{
+    public class Visa : ValidationAttribute
+    {
+        public Visa()
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            return Regex.IsMatch(value.ToString(), @"^4[0-9]{12}(?:[0-9]{3})?$");
+        }
+    }
+}
